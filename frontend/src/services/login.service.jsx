dde862bef48e2566d55ecb79b95c5b3e123ae5c7
eyo-as <eyosiyastumiso7@gmail.com>
2025-07-15 13:30:24 +0300

    remove unneccessary logs

--- frontend/src/services/login.service.jsx
+++ frontend/src/services/login.service.jsx
@@ -1,24 +1,23 @@
 const api_url = import.meta.env.VITE_API_URL;
 
 const logIn = async (formData) => {
-	const requestOptions = {
-		method: "POST",
-		headers: { "Content-Type": "application/json" },
-		body: JSON.stringify(formData),
-	};
-	console.log("About to send request");
-	console.log(requestOptions.body);
-	const response = await fetch(`${api_url}/api/employee/login`, requestOptions);
-	return response;
+  const requestOptions = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(formData),
+  };
+
+  const response = await fetch(`${api_url}/api/employee/login`, requestOptions);
+  return response;
 };
 
 // A function to log out the user
 const logOut = () => {
-	localStorage.removeItem("employee");
+  localStorage.removeItem("employee");
 };
 
 // Export the functions
 export default {
-	logIn,
-	logOut,
+  logIn,
+  logOut,
 };
